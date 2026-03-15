e5eb6a0cd013befcdcb7ee4a7688c6b3cfe6e52e
Sonny Li <sonnynomnom@gmail.com>
2026-03-15 03:03:17 -0400

    Update wordle.js

--- march-2026/3-02-wordle/wordle.js
+++ march-2026/3-02-wordle/wordle.js
@@ -4,11 +4,14 @@
 function wordleGuess(secret, guess) {
 
   let count = 0;
+  
   for (let i = 0; i < secret.length; i++) {
     if(secret[i] === guess[i]) {
       count++
     }
   }
+  
   return count
 }
+
 console.log(wordleGuess("CODEX", "COINS"));
